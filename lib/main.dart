ab09b2dd411bcaa789aea5a67e53a3f92ac18e32
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-07-13 14:58:59 +0200

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:first_flutter_app/Grab_form_value.dart';
+import 'package:first_flutter_app/storing_form_data.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,13 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: Scaffold(
-          body: MyGrabForm(),
-        ));
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FlutterDemo(storage: UserInputStorage()),
+    );
   }
 }
