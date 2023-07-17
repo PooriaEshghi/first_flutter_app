5e54f11c8d3861cc0f927ab49208c1b8a7b76c48
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-07-17 12:16:20 +0200

    HTTP Request via User Form

---
+++ lib/http-request/http_request.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(userId: json['userId'], id: json['id'], title: json['title']);
+  }
+}
+
+Future<Album> fetchAlbum(int albumId) async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'));
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album!');
+  }
+}
