e4660040b7ab136688a1762334cd1e7fd01a517f
pooria eshghi malayeri <100807060+PooriaEshghi@users.noreply.github.com>
2023-07-18 17:27:28 +0200

    adding update and delete Album

--- lib/http-request/http_request.dart
+++ lib/http-request/http_request.dart
@@ -19,12 +19,28 @@ class Album {
   }
 }
 
-Future<Album> fetchAlbum(int albumId) async {
+Future<Album> fetchAlbum() async {
   final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/$albumId'));
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
   if (response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load album!');
+  }
+}
+
+Future<Album> updateAlbum(String title) async {
+  final response = await http.put(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+      headers: <String, String>{
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }));
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to update!');
   }
 }
--- lib/http-request/myApp.dart
+++ lib/http-request/myApp.dart
@@ -1,126 +1,161 @@
-import 'package:first_flutter_app/http-request/http_sending_data.dart';
-import 'package:flutter/material.dart';
+// import 'package:first_flutter_app/http-request/http_sending_data.dart';
+import 'dart:async';
+import 'dart:convert';
 
-// import 'http_request.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+// class Album {
+//   final int userId;
+//   final int id;
+//   final String title;
+
+//   const Album({
+//     required this.userId,
+//     required this.id,
+//     required this.title,
+//   });
+
+//   factory Album.fromJson(Map<String, dynamic> json) {
+//     return Album(userId: json['userId'], id: json['id'], title: json['title']);
+//   }
+// }
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album!');
+  }
+}
+
+Future<Album> updateAlbum(String title) async {
+  final response = await http.put(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+      headers: <String, String>{
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }));
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to update!');
+  }
+}
+
+Future<Album> deleteAlbum(String id) async {
+  final response = await http.delete(
+    Uri.parse('https://jsonplaceholder.typicode.com/albums/$id'),
+    headers: <String, String>{
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  );
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to update album.');
+  }
+}
+
+class Album {
+  final int? id;
+  final String? title;
+
+  const Album({this.id, this.title});
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(id: json['id'], title: json['title']);
+  }
+}
+
+Future<Album> createAlbum(String title) async {
+  final response = await http.post(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+      headers: <String, String>{
+        'Content-type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String, String>{
+        'title': title,
+      }));
+  if (response.statusCode == 201) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Faild to create album!');
+  }
+}
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
   final TextEditingController _controller = TextEditingController();
-  Future<Album>? _futureAlbum;
+
+  // void _fetchAlbum() {
+  //   int albumId = int.tryParse(_controller.text) ?? 1;
+  //   setState(() {
+  //     futureAlbum = fetchAlbum(albumId);
+  //   });
+  // }
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('New Album')),
-        body: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(8.0),
-          child: (_futureAlbum == null) ? buildColumn() : BuildFutureBuilder(),
-        ));
-  }
-
-  Column buildColumn() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        TextField(
-          controller: _controller,
-          decoration: InputDecoration(labelText: 'title'),
+    return MaterialApp(
+      title: 'demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Geet HTTP Example'),
         ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              _futureAlbum = createAlbum(_controller.text);
-            });
-          },
-          child: const Text('Submit'),
-        )
-      ],
-    );
-  }
-
-  FutureBuilder<Album> BuildFutureBuilder() {
-    return FutureBuilder<Album>(
-      future: _futureAlbum,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text(snapshot.data!.title);
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-        return const CircularProgressIndicator();
-      },
+        body: Center(
+            child: Column(
+          children: [
+            FutureBuilder<Album>(
+                future: futureAlbum,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(snapshot.data!.title ?? 'RECORD DELETED'),
+                          TextField(
+                            controller: _controller,
+                            decoration: InputDecoration(
+                                hintText: 'Album ID to Delete:'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                futureAlbum = updateAlbum(_controller.text);
+                              });
+                            },
+                            child: Text('Confirm delete data'),
+                          ),
+                        ],
+                      );
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+                  }
+                  return const CircularProgressIndicator();
+                })
+          ],
+        )),
+      ),
     );
   }
 }
-
-
-
-
-
-// import 'package:first_flutter_app/http-request/http_request.dart';
-// import 'package:flutter/material.dart';
-
-// class MyApp extends StatefulWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-
-// class _MyAppState extends State<MyApp> {
-//   late Future<Album> futureAlbum;
-//   final TextEditingController _controller = TextEditingController();
-
-//   void _fetchAlbum() {
-//     int albumId = int.tryParse(_controller.text) ?? 1;
-//     setState(() {
-//       futureAlbum = fetchAlbum(albumId);
-//     });
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     futureAlbum = fetchAlbum(1);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'demo',
-//       theme: ThemeData(primarySwatch: Colors.blue),
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Geet HTTP Example'),
-//         ),
-//         body: Center(
-//             child: Column(
-//           children: [
-//             TextField(
-//               controller: _controller,
-//               keyboardType: TextInputType.number,
-//               decoration: InputDecoration(labelText: 'Album ID Number'),
-//             ),
-//             ElevatedButton(onPressed: _fetchAlbum, child: const Text('Submit')),
-//             FutureBuilder<Album>(
-//                 future: futureAlbum,
-//                 builder: (context, snapshot) {
-//                   if (snapshot.hasData) {
-//                     return Text(snapshot.data!.title);
-//                   } else if (snapshot.hasError) {
-//                     return Text('${snapshot.error}');
-//                   }
-//                   return const CircularProgressIndicator();
-//                 })
-//           ],
-//         )),
-//       ),
-//     );
-//   }
-// }
